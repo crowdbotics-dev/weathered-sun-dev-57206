33737520c65c5474d57dc83592afd6be023894da
Crowdbotics <engineering@crowdbotics.com>
2023-01-16 10:08:46 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/weathered_sun_dev_57206/MainActivity.java
@@ -0,0 +1,15 @@
+package com.weathered_sun_dev_57206;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "weathered_sun_dev_57206";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "weathered-sun-dev-57206",
+  url: "https://weathered-sun-dev-57206.botics.co",
+  api: "https://weathered-sun-dev-57206.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
